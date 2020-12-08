103a5b9e8790a4d16d60061c468e23f66ff2e7b9
Ken Adeniji <kenadeniji@hotmail.com>
2020-12-08 11:32:49 -0800

    Add files via upload

---
+++ IIS/WordEngineering/GeoLocation/HostIpInfo.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using InformationInTransit.ProcessLogic;
+
+public partial class WebServiceRequester_HostIpInfo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        HostIpLocationInfo hostIpLocationInfo = HostIpInfoHelper.GetLocationInfo();
+        latitude.Text = hostIpLocationInfo.Latitude.ToString();
+        longitude.Text = hostIpLocationInfo.Longitude.ToString();
+        countryName.Text = hostIpLocationInfo.CountryName.ToString();
+        countryCode.Text = hostIpLocationInfo.CountryCode.ToString();
+        suburb.Text = hostIpLocationInfo.Name.ToString();
+    }
+}
+
