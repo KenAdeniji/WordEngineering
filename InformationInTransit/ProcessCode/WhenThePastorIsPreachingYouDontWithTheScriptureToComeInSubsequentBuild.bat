d99b0d71dbffbb82bc3f3cb79f81939994f873cf
Ken Adeniji <kenadeniji@hotmail.com>
2020-12-08 17:27:02 -0800

    Add files via upload

---
+++ InformationInTransit/ProcessCode/WhenThePastorIsPreachingYouDontWithTheScriptureToComeInSubsequentBuild.bat
@@ -0,0 +1,1 @@
+csc WhenThePastorIsPreachingYouDontWithTheScriptureToComeInSubsequent.cs ..\ProcessLogic\StringHelper.cs /main:InformationInTransit.ProcessCode.WhenThePastorIsPreachingYouDontWithTheScriptureToComeInSubsequent /reference:"..\bin\Debug\HtmlAgilityPack.dll"
---
+++ InformationInTransit/ProcessCode/WroteToTheRomanEmpireAskingToReleasePaulAndYetSheJustWasntMovingBuild.bat
@@ -0,0 +1,1 @@
+csc WroteToTheRomanEmpireAskingToReleasePaulAndYetSheJustWasntMoving.cs ..\ProcessLogic\ObjectDumper.cs
