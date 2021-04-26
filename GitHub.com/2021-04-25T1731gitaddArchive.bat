d5e544ecc9ae881297c115b91aa4621198fbdb43
Ken Adeniji <kenadeniji@hotmail.com>
2021-04-25 17:40:15 -0700

    ../IIS/WordEngineering/Isaiah35v3-7/Isaiah35v3-7Archive.html

---
+++ GitHub.com/2021-04-25T1731gitaddArchive.bat
@@ -0,0 +1,11 @@
+        git add   ../IIS/WordEngineering/Isaiah35v3-7/Isaiah35v3-7Archive.html
+        git rm    ../IIS/WordEngineering/Isaiah35v3-7/Isaiah35v3_7.asmx
+        git rm    ../IIS/WordEngineering/Isaiah35v3-7/Isaiah35v3_7.cs
+        git rm    ../IIS/WordEngineering/Isaiah35v3-7/Isaiah35v3_7Build.bat
+        git rm    ../IIS/WordEngineering/Isaiah35v3-7/Isaiah35v3_7BuildArchive.bat
+
+        git add   ../GitHub.com/2021-04-25T1004gitadd.bat
+        git add   ../GitHub.com/2021-04-25T1004gitaddArchive.bat
+        git add   ../GitHub.com/2021-04-25T1731gitadd.bat
+        git add   ../GitHub.com/2021-04-25T1731gitaddArchive.bat
+		
