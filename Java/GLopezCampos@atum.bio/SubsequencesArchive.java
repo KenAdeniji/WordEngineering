1c2e1bc8b236cfe246664cc8b45353cc49eaa1d0
Ken Adeniji <kenadeniji@hotmail.com>
2021-05-26 07:07:22 -0700

    git add   Java/GLopezCampos@atum.bio/Subsequences.class         git add   Java/GLopezCampos@atum.bio/Subsequences.java         git add   Java/GLopezCampos@atum.bio/SubsequencesArchive.java

--- Java/GLopezCampos@atum.bio/SubsequencesArchive.java
+++ Java/GLopezCampos@atum.bio/SubsequencesArchive.java
@@ -1,17 +1,17 @@
-/** 
-Genesis Lopez
-Company	Atum
-Work e-mail	GLopezCampos@atum.bio
-Notes	2021-05-19T17:08:00 Thank you for applying to the Junior Software Engineer Internship at ATUM.
-The first round of our interview process is to write a function in Java per the following:
-Write a function in Java that takes as a parameter a string and returns a list of subsequences (from left to right) 
-that start with "ATG" and end with either "TAA", "TAG" or "TGA". 
-Example: Input: "GCATGCCGGTTACCTAAGGATGGGTTCAAAATAGCGG" 
-Output: [ "ATGCCGGTTACCTAA", "ATGGGTTCAAAATAG" ] 
-Please email me your answer within 4 business days of receiving this email. 
-We will be in touch after review regarding the next steps.
-Thank you again for applying to the internship. Genesis Lopez ATUM
-2021-05-19T17:21:00 ... 2021-05-19T19:51:00 Prove-of-concept. 
+/**
+	Genesis Lopez
+	Company Atum
+	Work e-mail GLopezCampos@atum.bio
+	Notes 2021-05-19T17:08:00 Thank you for applying to the Junior Software Engineer Internship at ATUM.
+	The first round of our interview process is to write a function in Java per the following:
+	Write a function in Java that takes as a parameter a string and returns a list of subsequences (from left to right)
+	that start with "ATG" and end with either "TAA", "TAG" or "TGA".
+	Example: Input: "GCATGCCGGTTACCTAAGGATGGGTTCAAAATAGCGG"
+	Output: [ "ATGCCGGTTACCTAA", "ATGGGTTCAAAATAG" ]
+	Please email me your answer within 4 business days of receiving this email.
+	We will be in touch after review regarding the next steps.
+	Thank you again for applying to the internship. Genesis Lopez ATUM
+	2021-05-19T17:21:00 ... 2021-05-19T19:51:00 Prove-of-concept.
 */
 
 import java.util.*;
@@ -23,80 +23,65 @@ class Subsequences
 	{
 		System.out.println
 		(
-			subsequences(args[0])	
-		);	
+			subsequences(args[0])
+		);
 	}
 
 	public static String subsequences(String input)
 	{
 		String startWithATGLiteral = "ATG";
 		int startWithATGIndex = 0;
-		
 		String[] endWithLiterals = {"TAA", "TAG", "TGA"};
-		int endWithLength = endWithLiterals.length;
-		int endWithIndex = 0;
-		int endWithIndexes = 0;
-		int endBlock = 0;
-
-		boolean endWithFound = false;	
-		
+		int[] endWithIndexes = {-1, -1, -1};
+		int endWithEitherIndex = input.length();
+		int endBlock = -1;
+		boolean endWithFound = false;
 		List<String> resultSet = new ArrayList<String>();
-		
 		String output;
-	
 		for (;;)
 		{
 			startWithATGIndex = input.indexOf(startWithATGLiteral, startWithATGIndex);
 			if (startWithATGIndex == -1)
 			{
 				break;
-			}	
-			for 
+			}
+			endWithFound = false;
+			for
 			(
-				endWithIndex = 0, 
-				endWithFound = false
-				;
+				int endWithIndex = 0, 
+					endWithLength = endWithIndexes.length;
 				endWithIndex < endWithLength;
 				++endWithIndex
 			)
 			{
-				endWithIndexes = input.indexOf
+				endWithIndexes[endWithIndex] =
+					input.indexOf
+					(
+						endWithLiterals[endWithIndex],
+						startWithATGIndex + 1
+					);
+				if
 				(
-					endWithLiterals[endWithIndex],
-					startWithATGIndex
-				);
-				if 
-				(
-					endWithIndexes > -1
+					endWithIndexes[endWithIndex] > startWithATGIndex &&
+					endWithIndexes[endWithIndex] < endWithEitherIndex
 				)
 				{
+					endWithEitherIndex = endWithIndexes[endWithIndex];
 					endWithFound = true;
-					endBlock = endWithIndexes + ((endWithLiterals[endWithIndex]).length());
-					break;
-				}		
+					endBlock = endWithEitherIndex + ((endWithLiterals[endWithIndex]).length());
+				}
 			}
-
 			if (!endWithFound)
 			{
 				endBlock = input.length();
-			}		
-
+			}
 			resultSet.add
 			(
-				'"' + 
-				input.substring
-				(
-					startWithATGIndex,
-					endBlock
-				)
-				+ '"'
+				'"' + input.substring(startWithATGIndex, endBlock) + '"'
 			);
-	
-			startWithATGIndex = endBlock;
+			++startWithATGIndex;
 		}
-		
 		output = "[" + String.join(", ", resultSet) + "]";
-		
 		return output;
 	}
 }
