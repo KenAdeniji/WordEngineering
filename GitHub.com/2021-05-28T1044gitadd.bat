a20014ffd919ff6ece51112cf6a1b0459d090115
Ken Adeniji <kenadeniji@hotmail.com>
2021-05-29 19:48:55 -0700

    git add    Batch/2017-01-02DoingSomething.bat         git add    GitHub.com/2021-05-28T1044gitadd.bat         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html

--- GitHub.com/2021-05-28T1044gitadd.bat
+++ GitHub.com/2021-05-28T1044gitadd.bat
@@ -3,10 +3,10 @@ git add         GitHub.com/2021-05-27T2134gitaddArchive.bat
 git add         GitHub.com/2021-05-28T1044gitadd.bat
 git add         GitHub.com/2021-05-28T1044gitaddArchive.bat
 		
-git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-27T0540LLangbert@quickbase.com_-_How are we modelable alike.txt
-git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-27T0540LLangbert@quickbase.com_-_How are we modelable alikeArchive.txt
-git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-28T1136The Minters Collective_-_Alex_-_TimothyMorgan_-_timothy@minters.art.txt
-git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-28T1136The Minters Collective_-_Alex_-_TimothyMorgan_-_timothy@minters.artArchive.txt
+git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-27T0540LLangbert@quickbase.com.txt
+git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-27T0540LLangbert@quickbase.comArchive.txt
+git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-28T1136timothy@minters.art.txt
+git add         IIS/WordEngineering/2018-05-03Correspondence/2021-05-28T1136timothy@minters.artArchive.txt
 		
 git add         IIS/WordEngineering/WordGroup/LaurenLangbert_-_LLangbert@quickbase.com_-_OurConnectionWithPeopleAreOurPastime.asmx
 git add         IIS/WordEngineering/WordGroup/LaurenLangbert_-_LLangbert@quickbase.com_-_OurConnectionWithPeopleAreOurPastime.html
