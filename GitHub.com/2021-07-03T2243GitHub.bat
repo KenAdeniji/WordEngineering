c7ce6256923b7ce89a7145ab7e31b5643d3d32f1
Ken Adeniji <kenadeniji@hotmail.com>
2021-07-03 22:55:02 -0700

    git add IIS/WordEngineering/Resume/KenAdenijiResume.docx git add IIS/WordEngineering/Resume/KenAdenijiResume.html git add IIS/WordEngineering/Resume/KenAdenijiResume.pdf git add	IIS/WordEngineering/Resume/KenAdenijiResumeArchive.html git add	IIS/WordEngineering/Resume/KenAdenijiResumeRelease2020-08-21.html git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
    git add GitHub.com/2021-07-03T2243GitHub.bat
    git add GitHub.com/2021-07-03T2243GitHubArchive.bat
    
    git add	IIS/WordEngineering/WordCross/

---
+++ GitHub.com/2021-07-03T2243GitHub.bat
@@ -0,0 +1,14 @@
+git add IIS/WordEngineering/Resume/KenAdenijiResume.docx
+git add IIS/WordEngineering/Resume/KenAdenijiResume.html
+git add IIS/WordEngineering/Resume/KenAdenijiResume.pdf
+git add	IIS/WordEngineering/Resume/KenAdenijiResumeArchive.html
+git add	IIS/WordEngineering/Resume/KenAdenijiResumeRelease2020-08-21.html
+git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+git add	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+		
+git add GitHub.com/2021-07-03T2243GitHub.bat
+git add GitHub.com/2021-07-03T2243GitHubArchive.bat
+		
+git add	IIS/WordEngineering/WordCross/
