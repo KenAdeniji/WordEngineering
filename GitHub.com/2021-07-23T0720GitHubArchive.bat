a668769ee5c11858591793bfdc65628fceeca9b1
Ken Adeniji <kenadeniji@hotmail.com>
2021-07-23 07:28:45 -0700

    2021-07-23T0720GitHub.bat

---
+++ GitHub.com/2021-07-23T0720GitHubArchive.bat
@@ -0,0 +1,9 @@
+        git add     "IIS/WordEngineering/Warner Bros/tours-api.dsp.wmg.com.html"
+        git add     IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+        git add     IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+        git add     IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+        git add     IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+git add        GitHub.com/2021-07-23T0720GitHub.bat
+git add        GitHub.com/2021-07-23T0720GitHubArchive.bat		
+git add        IIS/WordEngineering/2018-05-03Correspondence/2021-07-19T0746BoseAdenijiChicagoIllinois.txt
+git add        IIS/WordEngineering/2018-05-03Correspondence/2021-07-19T0746BoseAdenijiChicagoIllinoisArchive.txt
