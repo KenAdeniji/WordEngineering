4f336fe45f8b9b3f046e8dd1d0b88d79d8e84dab
Ken Adeniji <kenadeniji@hotmail.com>
2021-07-26 22:45:57 -0700

    2021-07-26T2242GitHub.bat

--- IIS/WordEngineering/JavaScript/OleksiiTrekhleb/OleksiiTrekhleb_-_JavaScriptAlgorithmsAndDataStructuresMain.js
+++ IIS/WordEngineering/JavaScript/OleksiiTrekhleb/OleksiiTrekhleb_-_JavaScriptAlgorithmsAndDataStructuresMain.js
@@ -1,8 +1,29 @@
 /*
 	2021-07-26T07:33:00 Created.
 */
-import { author, getBit } from "./modules/OleksiiTrekhleb_-_JavaScriptAlgorithmsAndDataStructuresModule.js";
-
 import * as Module from "./modules/OleksiiTrekhleb_-_JavaScriptAlgorithmsAndDataStructuresModule.js";
 
-resultSetElement.innerHTML = author;
+resultSetElement.innerHTML = 
+	`
+		<table>
+			<caption>Oleksii Trekhleb JavaScript Algorithms and Data Structures</caption> 
+ 			<thead>
+				<tr>
+					<th>Signature</th>				
+					<th>Example</th>
+					<th>Original</th>
+					<th>Custom</th>
+					<th>Validate</th>
+				</tr>
+			</thead>
+ 			<tbody>
+				<tr>
+					<td>getBit(number, bitPosition)</td>
+					<td>getBit(10, 2)</td>
+					<td>${Module.getBit(10, 2)}</td>
+					<td>${Module.getBitCustom(10, 2)}</td>
+					<td>${Module.getBit(10, 2) === Module.getBitCustom(10, 2)}</td>
+				</tr>
+			</tbody>
+		</table>	
+	`;	
--- IIS/WordEngineering/JavaScript/OleksiiTrekhleb/modules/OleksiiTrekhleb_-_JavaScriptAlgorithmsAndDataStructuresModuleArchive.js
+++ IIS/WordEngineering/JavaScript/OleksiiTrekhleb/modules/OleksiiTrekhleb_-_JavaScriptAlgorithmsAndDataStructuresModuleArchive.js
@@ -7,9 +7,14 @@
  * @param {number} bitPosition - zero based.
  * @return {number}
  */
-const author = "Oleksii Trekhleb";
-function getBit(number, bitPosition) {
+export function getBit(number, bitPosition) {
 	return (number >> bitPosition) & 1;
 }
 
-export { author, getBit };
+/*
+	2021-07-26T22:15:00	https://stackoverflow.com/questions/30429821/how-can-i-get-the-position-of-bits
+*/
+export function getBitCustom(number, bitPosition) {
+	var base2 = number.toString(2);
+	return parseInt(base2.charAt(base2.length - bitPosition - 1));
+}
