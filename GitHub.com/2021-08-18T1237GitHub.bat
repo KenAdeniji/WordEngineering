1002f42277166a356321004f98a7aaaf8e10fcac
Ken Adeniji <kenadeniji@hotmail.com>
2021-08-18 13:02:24 -0700

    2021-08-18T1237GitHub.bat

--- GitHub.com/2021-08-18T1237GitHub.bat
+++ GitHub.com/2021-08-18T1237GitHub.bat
@@ -4,10 +4,6 @@
         git add   IIS/WordEngineering/Resume/KenAdenijiResume.pdf
         git add   IIS/WordEngineering/Resume/KenAdenijiResumeArchive.html
         git add   IIS/WordEngineering/Resume/KenAdenijiResumeRelease2020-08-21.html
-        git rm    IIS/WordEngineering/Web Release2015-08-06T0604.config
-        git rm    IIS/WordEngineering/Web Release2015-08-08T1830.config
-        git rm    IIS/WordEngineering/Web Release2018-02-09T1856.config
-        git rm    IIS/WordEngineering/Web Release2020-12-29T1400.config
         git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
         git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
         git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
