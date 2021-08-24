7514bdaaa531b714856767d65ca75755f9c66c3c
Ken Adeniji <kenadeniji@hotmail.com>
2021-08-24 05:55:51 -0700

    2021-08-24T0551GitHub.bat

---
+++ GitHub.com/2021-08-24T0551GitHubArchive.bat
@@ -0,0 +1,9 @@
+        git add   IIS/WordEngineering/WordCross/WhatInstrumentOfValueDoIObtain.html
+        git add   IIS/WordEngineering/WordCross/WhatInstrumentOfValueDoIObtainArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
+
+git add        GitHub.com/2021-08-24T0551GitHub.bat
+git add        GitHub.com/2021-08-24T0551GitHubArchive.bat
