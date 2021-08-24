f1829b591e9a941e3a2b32a0fc292d4357cd4f4c
Ken Adeniji <kenadeniji@hotmail.com>
2021-08-23 22:34:18 -0700

    2021-08-23T2229GitHub.bat

---
+++ GitHub.com/2021-08-23T2229GitHub.bat
@@ -0,0 +1,11 @@
+        git add   Batch/2017-01-02DoingSomething.bat
+        git add   Batch/2017-01-02DoingSomethingArchive.bat
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+
+git add        GitHub.com/2021-08-23T2229GitHub.bat
+git add        GitHub.com/2021-08-23T2229GitHubArchive.bat
+git add        IIS/WordEngineering/WordCross/WhatInstrumentOfValueDoIObtain.html
+git add        IIS/WordEngineering/WordCross/WhatInstrumentOfValueDoIObtainArchive.html
