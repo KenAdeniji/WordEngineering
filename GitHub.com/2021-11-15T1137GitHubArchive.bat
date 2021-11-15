38693c6c33e0014df433487279b6bec39791511f
Ken Adeniji <kenadeniji@hotmail.com>
2021-11-15 11:40:24 -0800

    2021-11-15T1137GitHub.bat

---
+++ GitHub.com/2021-11-15T1137GitHubArchive.bat
@@ -0,0 +1,7 @@
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+        git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+
+git add        GitHub.com/2021-11-15T1137GitHub.bat
+git add        GitHub.com/2021-11-15T1137GitHubArchive.bat
