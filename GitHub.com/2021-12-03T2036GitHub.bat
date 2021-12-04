f19156dd61ff4d6c0c60f4024722cc853652bafc
Ken Adeniji <kenadeniji@hotmail.com>
2021-12-03 20:41:42 -0800

    2021-12-03T2036GitHub.bat

---
+++ GitHub.com/2021-12-03T2036GitHub.bat
@@ -0,0 +1,14 @@
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
+
+git add        GitHub.com/2021-12-03T2036GitHub.bat
+git add        IIS/WordEngineering/WordCross/WhatMagnitudeDidHePresent.asmx
+git add        IIS/WordEngineering/WordCross/WhatMagnitudeDidHePresent.html
+git add        IIS/WordEngineering/WordCross/WhatMagnitudeDidHePresentArchive.asmx
+git add        IIS/WordEngineering/WordCross/WhatMagnitudeDidHePresentArchive.html
