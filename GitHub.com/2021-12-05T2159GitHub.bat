2399d3a88ce358fe5df7353f27f0004b7ab9db6a
Ken Adeniji <kenadeniji@hotmail.com>
2021-12-05 22:07:48 -0800

    2021-12-05T2159GitHub.bat

---
+++ GitHub.com/2021-12-05T2159GitHub.bat
@@ -0,0 +1,17 @@
+        git add    IIS/WordEngineering/Resume/KenAdenijiResume.pdf
+        git add    IIS/WordEngineering/WordCross/WhatMagnitudeDidHePresent.html
+        git add    IIS/WordEngineering/WordCross/WhatMagnitudeDidHePresentArchive.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
+
+git add        GitHub.com/2021-12-05T2159GitHub.bat
+git add        IIS/WordEngineering/WordCross/ToObtainTheLastYouNeedToFavorTheFirst.asmx
+git add        IIS/WordEngineering/WordCross/ToObtainTheLastYouNeedToFavorTheFirst.html
+git add        IIS/WordEngineering/WordCross/ToObtainTheLastYouNeedToFavorTheFirstArchive.asmx
+git add        IIS/WordEngineering/WordCross/ToObtainTheLastYouNeedToFavorTheFirstArchive.html
