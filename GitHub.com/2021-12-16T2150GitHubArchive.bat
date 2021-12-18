17fbfa24383c18b02a98ef17bca43294c6b6d07f
Ken Adeniji <kenadeniji@hotmail.com>
2021-12-17 16:34:28 -0800

    2021-12-17T1630GitHub.bat

---
+++ GitHub.com/2021-12-16T2150GitHubArchive.bat
@@ -0,0 +1,19 @@
+        git add    Batch/2017-01-02DoingSomething.bat
+        git add    Batch/2017-01-02DoingSomethingArchive.bat
+        git add    "IIS/WordEngineering/Alameda County Health Agency Care Services Tri-City Community Support Center Behavioral Health Care Services/2021-12-06T1318MaureenOrphanos_DennisO'Brien_Covid-19BoosterShot.txt"
+        git add    "IIS/WordEngineering/Alameda County Health Agency Care Services Tri-City Community Support Center Behavioral Health Care Services/2021-12-06T1318MaureenOrphanos_DennisO'Brien_Covid-19BoosterShotArchive.txt"
+        git add    IIS/WordEngineering/Resume/KenAdenijiResume.html
+        git add    IIS/WordEngineering/Resume/KenAdenijiResume.pdf
+        git add    IIS/WordEngineering/Resume/KenAdenijiResumeArchive.html
+        git add    IIS/WordEngineering/Resume/KenAdenijiResumeRelease2020-08-21.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+
+git add        IIS/WordEngineering/GeoLocation/api.OpenWeatherMap.org.html
+git add        IIS/WordEngineering/GeoLocation/api.OpenWeatherMap.orgArchive.html
+git add        IIS/WordEngineering/JavaScript/TheJoeMorgan.com/
+git add        "PowerShell/PowerShell Networking Guide By Ed Wilson Created by Microsoft The Scripting Guy/"
+
+		
---
+++ GitHub.com/2021-12-17T1630GitHub.bat
@@ -0,0 +1,6 @@
+git add        GitHub.com/2021-12-16T2150GitHub.bat
+git add        GitHub.com/2021-12-16T2150GitHubArchive.bat
+git add        GitHub.com/2021-12-17T1630GitHub.bat
+git add        GitHub.com/2021-12-17T1630GitHubArchive.bat
+git add        IIS/WordEngineering/2018-05-03Correspondence/2021-12-17T1605UNCC_AbasifrekeEbong_YawoAmengonu.txt
+git add        IIS/WordEngineering/2018-05-03Correspondence/2021-12-17T1605UNCC_AbasifrekeEbong_YawoAmengonuArchive.txt
