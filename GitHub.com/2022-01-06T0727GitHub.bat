2c2ad75b576756c27867ef54ca576d2bfaf44193
Ken Adeniji <kenadeniji@hotmail.com>
2022-01-06 07:34:11 -0800

    2022-01-06T0727GitHub.bat

---
+++ GitHub.com/2022-01-06T0727GitHub.bat
@@ -0,0 +1,7 @@
+git add        modified:   Batch/2017-01-02DoingSomething.bat
+git add        modified:   GitHub.com/2022-01-04T1442GitHub.bat
+
+git add        GitHub.com/2022-01-06T0727GitHub.bat
+git add        IIS/WordEngineering/WordCross/WordComparison.html
+git add        IIS/WordEngineering/WordCross/WordComparisonArchive.html
+		
