ba73dfc082a0c6e0c822f4563401be952f06162b
Ken Adeniji <kenadeniji@hotmail.com>
2022-01-06 21:16:27 -0800

    git add        Python/AmitSaha/Flask_-_Matplotlib.figure_-_BibleBookChapter.py git add        Python/AmitSaha/Flask_-_Matplotlib.figure_-_BibleBookChapterArchive.py

--- Python/AmitSaha/Flask_-_Matplotlib.figure_-_BibleBookChapterArchive.py
+++ Python/AmitSaha/Flask_-_Matplotlib.figure_-_BibleBookChapterArchive.py
@@ -11,15 +11,21 @@ from matplotlib.figure import Figure
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def chartGraph():
     # Generate the figure **without using pyplot**.
 	fig = Figure()
 	bibleBooksTitlesList = ["Genesis", "Exodus", "Leviticus"]	
 	bibleBooksChaptersList = [50, 40]
-	bibleBooksChaptersList.append(27)	
+	bibleBooksChaptersList.append(27)
+	fig.suptitle("Bible Books Chapters")
+	fig.supxlabel('Books')
+	fig.supylabel('Chapters')
 	ax = fig.subplots()
 	ax.plot(bibleBooksTitlesList, bibleBooksChaptersList)
-	buf = BytesIO();fig.savefig(buf, format="png");data = base64.b64encode(buf.getbuffer()).decode("ascii");return f"<img src='data:image/png;base64,{data}'/>"
+	buf = BytesIO();
+	fig.savefig(buf, format="png");
+	data = base64.b64encode(buf.getbuffer()).decode("ascii");
+	return f"<img src='data:image/png;base64,{data}'/>"
 	
 if __name__ == "__main__":
     app.run()
