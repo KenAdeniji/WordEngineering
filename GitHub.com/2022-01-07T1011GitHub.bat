49d5fab1731e0f776474ea4899262b4b600be3c5
Ken Adeniji <kenadeniji@hotmail.com>
2022-01-07 10:22:28 -0800

    2022-01-07T1011GitHub.bat

---
+++ GitHub.com/2022-01-07T1011GitHub.bat
@@ -0,0 +1,4 @@
+git rm        Python/AmitSaha/Flask_-_Matplotlib.figure_-_EventDate
+git rm        Python/flask.pocoo.org/Flask_-_Matplotlib.figure_-_EventDateArchive
+
+git add GitHub.com/2022-01-07T1011GitHub.bat
