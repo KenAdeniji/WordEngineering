bda8b00fb79a4423e5ec2faed551d0af89ca9bd2
Ken Adeniji <kenadeniji@hotmail.com>
2022-01-06 19:38:01 -0800

    2022-01-06T1930GitHub.bat

---
+++ GitHub.com/2022-01-06T1930GitHub.bat
@@ -0,0 +1,7 @@
+git add        GitHub.com/2022-01-06T1930GitHub.bat
+git add        Python/AmitSaha/Flask_-_Matplotlib.figure.py
+git add        Python/AmitSaha/Flask_-_Matplotlib.figureArchive.py
+git add        Python/AmitSaha/Flask_-_Matplotlib.figure_-_BibleBookChapter.py
+git add        Python/AmitSaha/Flask_-_Matplotlib.figure_-_BibleBookChapterArchive.py
+git add        Python/AmitSaha/ListTupleBibleBook.py
+git add        Python/AmitSaha/ListTupleBibleBookArchive.py
---
+++ Python/AmitSaha/Flask_-_Matplotlib.figure_-_BibleBookChapter.py
@@ -0,0 +1,25 @@
+"""
+	2022-01-06Y18:26:00	Created.
+	2022-01-06Y18:26:00 http://matplotlib.org/devdocs/gallery/user_interfaces/web_application_server_sgskip.html
+"""
+import base64
+from io import BytesIO
+
+from flask import Flask
+from matplotlib.figure import Figure
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    # Generate the figure **without using pyplot**.
+	fig = Figure()
+	bibleBooksTitlesList = ["Genesis", "Exodus", "Leviticus"]	
+	bibleBooksChaptersList = [50, 40]
+	bibleBooksChaptersList.append(27)	
+	ax = fig.subplots()
+	ax.plot(bibleBooksTitlesList, bibleBooksChaptersList)
+	buf = BytesIO();fig.savefig(buf, format="png");data = base64.b64encode(buf.getbuffer()).decode("ascii");return f"<img src='data:image/png;base64,{data}'/>"
+	
+if __name__ == "__main__":
+    app.run()
