055191ac39c61f5c343d8cb6e47c0203836757d0
Ken Adeniji <kenadeniji@hotmail.com>
2022-01-18 16:26:36 -0800

    2022-01-18T1622GitHub.bat

---
+++ GitHub.com/2022-01-18T1622GitHub.bat
@@ -0,0 +1,4 @@
+git add		GitHub.com/2022-01-18T1622GitHub.bat
+git add		GitHub.com/2022-01-18T1622GitHubArchive.bat
+git add     IIS/WordEngineering/2018-05-03Correspondence/2022-01-18T1558JuliaRabinovichImmigrationRepresentationUnitSexualOffenderAnnualRegistration.txt
+git add     IIS/WordEngineering/2018-05-03Correspondence/2022-01-18T1558JuliaRabinovichImmigrationRepresentationUnitSexualOffenderAnnualRegistrationArchive.txt
