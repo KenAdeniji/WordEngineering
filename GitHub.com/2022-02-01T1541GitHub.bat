63fa9b865abe64617666b42112c1deba96fa35f0
Ken Adeniji <kenadeniji@hotmail.com>
2022-02-01 15:58:43 -0800

    2022-02-01T1541GitHub.bat

---
+++ GitHub.com/2022-02-01T1541GitHub.bat
@@ -0,0 +1,11 @@
+git add           IIS/WordEngineering/WordCross/DatesComputation.html
+git add           IIS/WordEngineering/WordCross/DatesComputationArchive.html
+git add        IIS/WordEngineering/WordCross/LetsBringThisToTheConstitutionMovement.html
+git add        IIS/WordEngineering/WordCross/LetsBringThisToTheConstitutionMovementArchive.html
+
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add    IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
+
+git add        GitHub.com/2022-02-01T1541GitHub.bat
