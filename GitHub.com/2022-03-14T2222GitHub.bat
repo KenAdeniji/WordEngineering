c0eddd4a7025c575f10fedab0533fe47f761b535
Ken Adeniji <kenadeniji@hotmail.com>
2022-03-14 22:25:00 -0700

    2022-03-14T2222GitHub.bat

---
+++ GitHub.com/2022-03-14T2222GitHub.bat
@@ -0,0 +1,8 @@
+        git add   IIS/WordEngineering/WordCross/InThisIObtainMyUse.html
+        git add   IIS/WordEngineering/WordCross/InThisIObtainMyUseArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
+
+git add        GitHub.com/2022-03-14T2222GitHub.bat
