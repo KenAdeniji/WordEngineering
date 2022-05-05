8431009fda3e8cea4c1f79714be9584379aefdc9
Ken Adeniji <kenadeniji@hotmail.com>
2022-05-05 14:22:54 -0700

    2022-05-05T1403GitHub.bat

--- GitHub.com/2022-05-05T1403GitHub.bat
+++ GitHub.com/2022-05-05T1403GitHub.bat
@@ -3,3 +3,7 @@ git add        IIS/WordEngineering/WordCross/ADayToSubmitATimeToLookElsewhere.as
 git add        IIS/WordEngineering/WordCross/ADayToSubmitATimeToLookElsewhere.html
 git add        IIS/WordEngineering/WordCross/ADayToSubmitATimeToLookElsewhereArchive.asmx
 git add        IIS/WordEngineering/WordCross/ADayToSubmitATimeToLookElsewhereArchive.html
+git add		   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
