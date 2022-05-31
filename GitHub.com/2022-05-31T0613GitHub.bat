8a4cf2367980ca6ebd36e3086d148d23892f1d23
Ken Adeniji <kenadeniji@hotmail.com>
2022-05-31 06:17:07 -0700

    2022-05-31T0613GitHub.bat

---
+++ GitHub.com/2022-05-31T0613GitHub.bat
@@ -0,0 +1,7 @@
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
+git add        GitHub.com/2022-05-31T0613GitHub.bat
+git add        IIS/WordEngineering/2018-05-03Correspondence/2022-05-31T0527AlexTinmingLee_FremontCalifornia_Assemblymembers_CitizenshipApplication.txt
+git add        IIS/WordEngineering/2018-05-03Correspondence/2022-05-31T0527AlexTinmingLee_FremontCalifornia_Assemblymembers_CitizenshipApplicationArchive.txt
