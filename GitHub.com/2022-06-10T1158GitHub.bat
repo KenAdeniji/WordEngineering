18676eb8fa034bf165b1b0bf22c489b5c8200099
Ken Adeniji <kenadeniji@hotmail.com>
2022-06-10 15:27:32 -0700

    2022-06-10T1158GitHub.bat

--- GitHub.com/2022-06-10T1158GitHub.bat
+++ GitHub.com/2022-06-10T1158GitHub.bat
@@ -3,5 +3,7 @@
         git add   IIS/WordEngineering/WordCross/TheDangersOfComputationalMissionArchive.html
         git add   IIS/WordEngineering/WordUnion/BibleWord.html
         git add   IIS/WordEngineering/WordUnion/BibleWordArchive.html
+        git add   IIS/WordEngineering/WordUnion/Exact.html
+        git add   IIS/WordEngineering/WordUnion/ExactArchive.html
 
 git add        GitHub.com/2022-06-10T1158GitHub.bat
