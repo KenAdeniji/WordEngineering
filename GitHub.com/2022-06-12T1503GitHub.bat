d77867592c3a9d616c30dd1f2d172d98b8738581
Ken Adeniji <kenadeniji@hotmail.com>
2022-06-12 15:36:20 -0700

    2022-06-12T1503GitHub.bat

--- GitHub.com/2022-06-12T1503GitHub.bat
+++ GitHub.com/2022-06-12T1503GitHub.bat
@@ -1,2 +1,6 @@
 git add		GitHub.com/2022-06-12T1503GitHub.bat
 git add     "IIS/WordEngineering/CascadingStyleSheet/Alexis Goldstein/AlexisGo.github.com/LearningCSSAnimations/"
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
