a626ed91a5e19acc7c493171375bc147ec3578b1
Ken Adeniji <kenadeniji@hotmail.com>
2022-07-02 20:16:33 -0700

    2022-07-02T1614GitHub.bat

--- GitHub.com/2022-07-02T1614GitHub.bat
+++ GitHub.com/2022-07-02T1614GitHub.bat
@@ -2,6 +2,13 @@
         git add   IIS/WordEngineering/Resume/KenAdenijiResume.pdf
         git add   IIS/WordEngineering/Resume/KenAdenijiResumeArchive.html
         git add   IIS/WordEngineering/Resume/KenAdenijiResumeRelease2020-08-21.html
+
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.txt
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionArchive.html
+        git add   IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescriptionRelease2020-11-26.html
+		
         git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
         git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
         git add   IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.txt
