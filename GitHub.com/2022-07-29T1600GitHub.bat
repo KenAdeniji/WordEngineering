e0aac8825405a98388f171a00fcec5be0b8300e7
Ken Adeniji <kenadeniji@hotmail.com>
2022-07-30 17:47:27 -0700

    git add        GitHub.com/2022-07-29T1600GitHub.bat         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.txt         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html

--- GitHub.com/2022-07-29T1600GitHub.bat
+++ GitHub.com/2022-07-29T1600GitHub.bat
@@ -20,3 +20,8 @@ git add        IIS/WordEngineering/WordCross/TwoEighteenTwoNineteenAndForLongCas
 git add IIS/WordEngineering/WordCross/TwoEighteenTwoNineteenAndForLongCastArchive.asmx
 git add        IIS/WordEngineering/WordCross/TwoEighteenTwoNineteenAndForLongCastArchive.html
 
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.txt
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
+        git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
