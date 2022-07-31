febdf4ed09954040202f532d9c04d4e5bf3b2a73
Ken Adeniji <kenadeniji@hotmail.com>
2022-07-31 15:00:12 -0700

    2022-07-29T1600GitHub.bat git add        IIS/WordEngineering/WordCross/SetALineHereTulan.html git add		IIS/WordEngineering/WordCross/SetALineHereTulanArchive.html

--- GitHub.com/2022-07-29T1600GitHub.bat
+++ GitHub.com/2022-07-29T1600GitHub.bat
@@ -25,3 +25,6 @@ git add        IIS/WordEngineering/WordCross/TwoEighteenTwoNineteenAndForLongCas
         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.txt
         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationArchive.html
         git add    IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertationRelease2020-12-17.html
+
+git add        IIS/WordEngineering/WordCross/SetALineHereTulan.html
+git add		IIS/WordEngineering/WordCross/SetALineHereTulanArchive.html
