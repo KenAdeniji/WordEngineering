cecf77906f56f3c97052304b1e996f3d719aae8c
Ken Adeniji <kenadeniji@hotmail.com>
2023-01-27 07:16:16 -0800

    git add    IIS/WordEngineering/OperatingSystem/UNIXcut.html         git add   IIS/WordEngineering/Resume/KenAdenijiResume.html         git add   IIS/WordEngineering/Resume/KenAdenijiResume.pdf         git add   IIS/WordEngineering/Resume/KenAdenijiResume.txt
    git add GitHub.com/2023-01-26T2000GitHub.bat
    git add GitHub.com/2023-01-27T0007GitHub.bat
    git add IIS/WordEngineering/OperatingSystem/UNIXsort.html

---
+++ GitHub.com/2023-01-26T2000GitHub.bat
@@ -0,0 +1,56 @@
+git mv -f  IIS/WordEngineering/WordUnion/DateAdd.aspx IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateAdd.html IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateApart.aspx IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateApart.aspx.cs IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateCompute.aspx IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateCompute.aspx.cs IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateDifference.aspx IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateDifference.aspx.cs IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateDifference.html IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/Dater.html IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateReverse.html IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DaterWebService.asmx IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordCross/DateRatio.html IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordCross/DatesComputation.html IIS/WordEngineering/Dated/DateComputation.html
+
+git rm -f  IIS/WordEngineering/Date*Archive.html
+
+git mv -f  IIS/WordEngineering/WordGroup/AFitInTime.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordGroup/AndThenAtElevenOClockWeAreGoingToGiveYouPreparingOnPollution_ArmorProgrammingBeganInFranceFifteenYearsAgo.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordUnion/BecauseWeAreHellThisIsOurDefinition.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordGroup/BeginAtLeastAndYouFindTheSufficiencyOfGod.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/JavaScript/Algorithm/ChooseYourProphecyAndItIsYourDecision.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/JavaScript/Algorithm/ChooseYourProphecyAndItIsYourDecision.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordUnion/DateAdd.aspx IIS/WordEngineering/Dated
+git mv -f  IIS/WordEngineering/WordUnion/DateAdd.aspx.cs IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordUnion/DateAdd.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordGroup/IWillFeelAWinningAge.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordGroup/Lent.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordGroup/MondayFifteenthNineteenSeventyThree.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordUnion/That'sWhatWeGotToDo.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordUnion/UnixEpochDateTime.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordCross/WhatWillDriveYouToHate.html IIS/WordEngineering/Dated
+
+git mv -f  IIS/WordEngineering/WordCross/WhatWillYouFindThatPlace.html IIS/WordEngineering/Dated
+
+git add IIS/WordEngineering/Dated/UnixEpochDateTime.html
+
+git add IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+git add IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+git add IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.txt
+
+git add	GitHub.com/2023-01-26T2000GitHub.bat
---
+++ GitHub.com/2023-01-27T0007GitHub.bat
@@ -0,0 +1,8 @@
+       git add    IIS/WordEngineering/OperatingSystem/UNIXcut.html
+        git add   IIS/WordEngineering/Resume/KenAdenijiResume.html
+        git add   IIS/WordEngineering/Resume/KenAdenijiResume.pdf
+        git add   IIS/WordEngineering/Resume/KenAdenijiResume.txt
+
+git add GitHub.com/2023-01-26T2000GitHub.bat
+git add GitHub.com/2023-01-27T0007GitHub.bat
+git add IIS/WordEngineering/OperatingSystem/UNIXsort.html
