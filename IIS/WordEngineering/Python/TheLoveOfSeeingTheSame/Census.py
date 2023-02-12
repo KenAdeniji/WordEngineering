303773c32fc1b07f16d9c50b9a07d030d256fe9a
Ken Adeniji <kenadeniji@hotmail.com>
2023-02-12 13:02:01 -0800

    Census

--- IIS/WordEngineering/Python/TheLoveOfSeeingTheSame/Census.py
+++ IIS/WordEngineering/Python/TheLoveOfSeeingTheSame/Census.py
@@ -3,7 +3,9 @@
 	2023-02-11T18:22:00	http://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
 	print(firstCensusReuben)
 	print(len(censuses))
-	Numbers 1:46 six hundred thousand and three thousand and five hundred and fifty
+	Numbers 1:46 six hundred thousand and three thousand and five hundred and fifty 603550
+	2023-02-12T12:50:00	http://markheath.net/post/python-equivalents-of-linq-methods
+	2023-02-12T12:50:00	http://stackoverflow.com/questions/12734839/linq-like-sum-function-in-python/12734852
 '''
 import StringHelper
 
@@ -32,3 +34,6 @@ if __name__ == '__main__':
 	firstCensusDan = Census(1, "Dan", 62700, "Numbers 1:39")
 	firstCensusAsher = Census(1, "Asher", 41500, "Numbers 1:39")
 	firstCensusNaphtali = Census(1, "Naphtali", 53400, "Numbers 1:41")
+	
+	censusPopulation = sum(x.population for x in censuses)
+	print("Census Population: ", censusPopulation)
