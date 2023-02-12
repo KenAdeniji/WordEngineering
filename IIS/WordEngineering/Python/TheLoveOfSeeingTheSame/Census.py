28a8ab69e9b1237ff4a802636cedd8a89a62c6c9
Ken Adeniji <kenadeniji@hotmail.com>
2023-02-11 19:16:37 -0800

    Census

--- IIS/WordEngineering/Python/TheLoveOfSeeingTheSame/Census.py
+++ IIS/WordEngineering/Python/TheLoveOfSeeingTheSame/Census.py
@@ -2,7 +2,7 @@
 	2023-02-11T09:32:00 Created.
 	2023-02-11T18:22:00	http://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
 	print(firstCensusReuben)
-	print(len(firstCensus))
+	print(len(census))
 '''
 import StringHelper
 
@@ -12,20 +12,16 @@ class Census:
         self.tribe = tribe
         self.population = population
         self.scriptureReference = scriptureReference
-        match iteration:
-            case 1:
-                firstCensus.append(self)
-            case 2:
-                secondCensus.append(self)
+        census.append(self)
 
     def __str__(self):
         return StringHelper.positionTitle(self.iteration) + " " + self.tribe + " " + str(self.population) + " (" + str(self.scriptureReference) + ")"
 
 if __name__ == '__main__':
-	firstCensus = []
-	secondCensus = []
+	census = []
 	firstCensusReuben = Census(1, "Reuben", 46500, "Numbers 1:21")
 	firstCensusSimeon = Census(1, "Simeon", 59300, "Numbers 1:23")
 	firstCensusGad = Census(1, "Gad", 45650, "Numbers 1:25")
 	firstCensusJudah = Census(1, "Judah", 74600, "Numbers 1:27")
 	firstCensusIssachar = Census(1, "Issachar", 54400, "Numbers 1:29")
+	
