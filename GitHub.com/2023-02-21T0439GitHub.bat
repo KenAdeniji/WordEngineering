4b01b70e208a267436c403fd3a796a4e712a4882
Ken Adeniji <kenadeniji@hotmail.com>
2023-02-22 14:48:22 -0800

    2023-02-21T0439GitHub.bat

---
+++ GitHub.com/2023-02-21T0439GitHub.bat
@@ -0,0 +1,11 @@
+git add   	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.html
+git add   	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.pdf
+git add   	IIS/WordEngineering/WordUnion/2015-10-23DoctoralDissertation.txt
+
+git add	IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.html
+git add IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.pdf
+git add IIS/WordEngineering/WordUnion/2019-10-05ArtifactDescription.txt
+
+git add GitHub.com/2023-02-21T0439GitHub.bat
+git add IIS/WordEngineering/WordCross/IwoKanSebe.asmx
+git add IIS/WordEngineering/WordCross/IwoKanSebe.html
