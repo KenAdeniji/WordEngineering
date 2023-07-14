10d352c3e150fde13759594f7853b28480388362
Ken Adeniji <kenadeniji@hotmail.com>
2023-07-13 21:10:31 -0700

    2023-07-13T20:50:00 ... 2023-07-13T21:08:00 http://www.20lessons.com/ 20 Lessons Book Series By Robin Nixon
    Build
    real-world skills in
    less than an hour for each lesson,
    and follow up to five hours of video tutorials.
    
    Learning web development is now easier than it's ever been
    
    Top-selling author Robin Nixon has written over 30 books on computing, many of which are used as standard references in colleges and universities worldwide. Now Robin brings together his expertise in web development into a new series of titles, each of which will teach you the subject in under 20 lessons of less than an hour each, and that’s including the comprehensive online video tutorial that accompanies each lesson. All the books also come with a collection of example files so you can load them onto your own computer to try out for yourself (mouse over or click the book covers above).
    
    Buy from Amazon and save up to 30%!
    
        Buy CSS & CSS3: 20 Lessons to Successful Web Development / Kindle Version
        Buy HTML5: 20 Lessons to Successful Web Development / Kindle Version
        Buy JavaScript: 20 Lessons to Successful Web Development / Kindle Version
        Buy PHP: 20 Lessons to Successful Web Development / Kindle Version
    
    Video Tutorials
    
    There are 20 videos per book, comprising between 4 and 5 hours of additional tutorials based around the examples in the books. Altogether there are 80 videos containing almost 20 hours of material.

--- InformationInTransit/ProcessCode/ThisIsHowIHaveDesireToUseYou.cs
+++ InformationInTransit/ProcessCode/ThisIsHowIHaveDesireToUseYou.cs
@@ -34,11 +34,35 @@ namespace InformationInTransit.ProcessCode
 		Larry Page, co-founder Google.
 			http://en.wikipedia.org/wiki/PageRank
 		2023-07-13T15:49:00 WHERE	Word IS NOT NULL
-		2023-07-1315:59:00 ... 2023-07-1316:16:00
+		2023-07-13T15:59:00 ... 2023-07-1316:16:00
 			http://www.blackwasp.co.uk/soundex.aspx
 			http://www.blackwasp.co.uk/soundex_2.aspx
-		2023-07-1316:26:00 0.0 RankImportance	
-		2023-07-1320:43:00 http://stackoverflow.com/questions/31077216/how-to-sort-datatable-with-multi-column-using-linq/31077345#31077345
+		2023-07-13T16:26:00 0.0 RankImportance	
+		2023-07-13T20:43:00 http://stackoverflow.com/questions/31077216/how-to-sort-datatable-with-multi-column-using-linq/31077345#31077345
+		2023-07-13T20:50:00 ... 2023-07-13T21:08:00 http://www.20lessons.com/
+20 Lessons Book Series
+By Robin Nixon
+
+
+Build
+real-world skills in
+less than an hour for each lesson,
+and follow up to five hours of video tutorials.
+
+Learning web development is now easier than it's ever been
+
+Top-selling author Robin Nixon has written over 30 books on computing, many of which are used as standard references in colleges and universities worldwide. Now Robin brings together his expertise in web development into a new series of titles, each of which will teach you the subject in under 20 lessons of less than an hour each, and that’s including the comprehensive online video tutorial that accompanies each lesson. All the books also come with a collection of example files so you can load them onto your own computer to try out for yourself (mouse over or click the book covers above).
+
+Buy from Amazon and save up to 30%!
+
+    Buy CSS & CSS3: 20 Lessons to Successful Web Development / Kindle Version
+    Buy HTML5: 20 Lessons to Successful Web Development / Kindle Version
+    Buy JavaScript: 20 Lessons to Successful Web Development / Kindle Version
+    Buy PHP: 20 Lessons to Successful Web Development / Kindle Version
+
+Video Tutorials
+
+There are 20 videos per book, comprising between 4 and 5 hours of additional tutorials based around the examples in the books. Altogether there are 80 videos containing almost 20 hours of material.
 	*/
 	public class ThisIsHowIHaveDesireToUseYou
 	{
