56725ed93d58f177e900573b0343cf739ab829c0
Ken Adeniji <kenadeniji@hotmail.com>
2023-10-15 08:02:49 -0700

    2023-10-13T06:11:00...2023-10-13T07:36:00 google.com +python object list html table site:stackoverflow.com 	2023-10-13T07:36:00 Urine 	2023-10-13T07:36:00...2023-10-13T07:56:00 html table while iteration.

--- IIS/WordEngineering/Python/JasonCMcDonald/DeadSimplePython/bible_book_io.py
+++ IIS/WordEngineering/Python/JasonCMcDonald/DeadSimplePython/bible_book_io.py
@@ -4,6 +4,9 @@
 2023-10-13T21:29:00	It separates the serialization from the file write.
 	json_string = json.dumps([ob.__dict__ for ob in list_bible_book])
 	path_JSON_file = Path("BibleBook.json");
+	2023-10-13T06:11:00...2023-10-13T07:36:00 google.com +python object list html table site:stackoverflow.com 
+	2023-10-13T07:36:00 Urine
+	2023-10-13T07:36:00...2023-10-13T07:56:00 html table while iteration.	
 """
 import sys
 from pathlib import Path
@@ -25,7 +28,18 @@ if __name__ == '__main__':
 	
 	json_string = json.dumps([ob.__dict__ for ob in list_bible_book])
 	path_JSON_file = Path("BibleBook.json");
-	
 	with open(path_JSON_file, "w") as file:
 	    file.write(json_string)
-		
+
+	index = 0
+	length = len(list_bible_book)
+	html_string = "<table><thead><tr><th>ID</th><th>Title</th></tr></thead><tbody>";
+	while index < length:
+		html_string += f"<tr><td>{list_bible_book[index].ID}</td><td>{list_bible_book[index].Title}</td></tr>"
+		index += 1
+	html_string += "</tbody></table>"	
+	path_HTML_file = Path("BibleBook.html");
+	with open(path_HTML_file, "w") as file:
+	    file.write(html_string)
+
+
