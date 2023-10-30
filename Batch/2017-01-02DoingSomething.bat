d14124f8f1f253fd255ba62e60615de32da3b7e7
Ken Adeniji <kenadeniji@hotmail.com>
2023-10-29 21:20:54 -0700

    <li id="bibliographyRankins2015" datetime="2020-06-19T00:00:00"> 					<cite>Microsoft SQL Server 2014 Unleashed</cite> 					Rankins, R., Bertucci, P., Gallelli, C., Silverstein, A. T.; (2015) 					ISBN-13: 978-0672337291 					ISBN-10: 0672337290 				</li>

--- Batch/2017-01-02DoingSomething.bat
+++ Batch/2017-01-02DoingSomething.bat
@@ -183,7 +183,7 @@ DBCC CHECKIDENT ('WordEngineering..Dream', RESEED, 5138);
 DBCC CHECKIDENT ('WordEngineering..Event', RESEED, 1454);
 DBCC CHECKIDENT ('WordEngineering..HisWord', RESEED, 154962);
 DBCC CHECKIDENT ('WordEngineering..QuestionAndAnswer', RESEED, 2963);
-DBCC CHECKIDENT ('WordEngineering..Remember', RESEED, 156052);
+DBCC CHECKIDENT ('WordEngineering..Remember', RESEED, 160670);
 DBCC CHECKIDENT ('WordEngineering..SacredText', RESEED, 606);
 DBCC CHECKIDENT ('WordEngineering..Software', RESEED, 3595);
 DBCC CHECKIDENT ('WordEngineering..StreetAddress', RESEED, 4517);
