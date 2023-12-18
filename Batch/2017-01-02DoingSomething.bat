90c2c6a71bf1abc1c7f85a17a66e4e487d39224d
Ken Adeniji <kenadeniji@hotmail.com>
2023-12-18 06:37:31 -0800

    2023-12-18T04:45:00...2023-12-18T06:33:00 				http://wirfs-brock.com/PDFs/RebeccaWirfs-BrockResume.pdf 				Corrected anchor id. 		--> 		<title>Ken Adeniji Resume</title> 		<meta name="description" content="Ken Adeniji Resume"/> 		<meta name="keywords" content="C#, SQL Server, Javascript, HTML5, CSS, ASP.NET, .NET, UML, WebAssembly, Node, React, Angular, C++, C, Visual Basic, Java, Go, PowerBuilder,  Python, R Language, GraphQL, PHP, Ruby, Scalable Vector Graphics (SVG), GitHub, WordPress, JSON, XML. jQuery"/> 		<meta name="robots" content="index, follow" /> 		<meta name="author" content="Ken Adeniji" /> 		<meta name="viewport" content="width=device-width, initial-scale=1"> 		<link rel="shortcut icon" href="http://e-comfort.ephraimtech.com/WordEngineering/Images/ephraim.jpg">     </head>     <body> 		<article> 		<header> 			<div style="text-align:center" itemscope itemtype="http://schema.org/Person"> 				<h1 itemprop="name">Ken Adeniji</h1> 				<address> 					<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"> 						<span itemprop="streetAddress">4762 Canvasback Common</span><br> 						<span itemprop="addressLocality">Fremont</span>, 						<span itemprop="addressRegion">California</span> 						<span itemprop="postalCode">94555</span><br> 					</div> 					<a href="tel:+15107968121" itemprop="telephone">(510) 796-8121</a><br> 					<a href="mailto:KenAdeniji@hotmail.com" itemprop="email">KenAdeniji@hotmail.com</a><br> 					<a href="http://www.JesusInTheLamb.com" itemprop="url" datetime="2004-11-10T00:00:00">www.JesusInTheLamb.com</a><br> 				</address> 			</div> 			<div style="text-align:center"> 				<a href="http://e-comfort.ephraimtech.com/WordEngineering/Resume/GeoLocationPostalAddress.html">GeoLocation Postal Address</a> 			</div> 			<nav> 				<ul> 					<li><a href="#objective">Objective</a> 					<li><a href="#careerSummary">Career Summary</a> 					<li><a href="#USPTOUnitedStatesPatentAndTrademarkOffice">USPTO - United States Patent and Trademark Office</a> 					<li><a href="#jobDescriptions">Job Descriptions</a> 					<li><a href="#otherWorkSummary">Other Work Summary</a> 					<li><a href="#educationSummary">Education Summary</a> 					<li><a href="#skillsSummary">Skills Summary</a> 				</ul> 			</nav> 		</header>
    		<section>
            <h2 id="objective">OBJECTIVE</h2>
            <p>
                A software engineer role in an organization that will offer an opportunity to develop products and services that will add-value to the customers, shareholders, and community.
            </p>
    		</section>
    
            <section>
    		<h2 id="careerSummary">CAREER SUMMARY</h2>
            <ul>
    			<li datetime="2023-12-18T04:45:00">
    				Since 2002, I have been the designer and first implementer of
    				<a href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/GetAPage.html">AlphabetSequence</a>.
    				Since 2000, I have written articles and software to explain the word of God.
    			</li>

--- Batch/2017-01-02DoingSomething.bat
+++ Batch/2017-01-02DoingSomething.bat
@@ -425,7 +425,7 @@ ORDER BY HisWordID DESC
 2023-10-30T19:43:00	http://craigslist.org
 
 2023-12-17T11:29:00 URIMaintenanceWebForm.aspx
-javascriptweekly.com/issues 	JavaScript Weekly 		JavaScript 	2021-02-06 	54919
+LearnDapper.com 	Welcome To Learn Dapper This site is for developers who want to learn how to use Dapper - the micro ORM produced by the people behind Stackoverflow. What is Dapper? link Dapper is a popular simple object mapping tool. It is designed primarily to be used in scenarios where you want to work with data in a strongly typed fashion - as business objects in a .NET application, but don't want to spend hours writing code to map query results from ADO.NET data readers to instances of those objects. Dapper is an open source project under the Apache license. It is available as a Nuget package and has been downloaded over 16 million times. Is Dapper an ORM? link Dapper falls into a family of tools known as micro-ORMs. These tools perform only a subset of the functionality of full-blown Object Relations Mappers, such as Entity Framework Core. Features vary by product. The following table provides a general idea of the capabilities that you can expect to find in a micro ORM compared to an ORM: 		Twin sibling 	2021-01-21 	54900
 
 2023-11-16T14:59:00...2023-11-16T16:42:00
 	http://www.commbank.com.au
