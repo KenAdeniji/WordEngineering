5cc7a42e47d336d564b97490c71b5b42847bb8d2
Ken Adeniji <kenadeniji@hotmail.com>
2024-06-30 23:14:43 -0700

    @ECHO OFF REM 2024-06-30T18:24:00 http://www.google.com/books/edition/The_Book_of_Batch_Scripting/SfLNEAAAQBAJ?hl=en&gbpv=1 REM 2024-06-30T18:31:00 http://stackoverflow.com/questions/17605767/create-a-list-or-an-array-and-print-each-item-in-windows-batch
    set firstCensus=46500;^
    59300;^
    45650;^
    74600;^
    54400;
    
    set total=0
    (for %%a in (%firstCensus%) do (
       echo %%a
       set /A total=total + %%a
    ))
    echo %total%
    pause

--- Batch/2017-01-02DoingSomething.bat
+++ Batch/2017-01-02DoingSomething.bat
@@ -188,7 +188,7 @@ DBCC CHECKIDENT ('URI..UriTechnology', RESEED, 2);
 DBCC CHECKIDENT ('URI..UriWordEngineering', RESEED, 62859);
 
 DBCC CHECKIDENT ('WordEngineering..ActToGod', RESEED, 1766);
-DBCC CHECKIDENT ('WordEngineering..APass', RESEED, 7589);
+DBCC CHECKIDENT ('WordEngineering..APass', RESEED, 7722);
 DBCC CHECKIDENT ('WordEngineering..CaseBasedReasoning', RESEED, 159389);
 DBCC CHECKIDENT ('WordEngineering..ClassAssociates', RESEED, 22952);
 DBCC CHECKIDENT ('WordEngineering..Contact', RESEED,  15052);
--- Batch/JackMcLarney/2024-06-30T1824_-_JackMcLarney_-_TheBookOfBatchScriptingFromFundamentalsToAdvancedAutomation2024-06-24_-_Census.bat
+++ Batch/JackMcLarney/2024-06-30T1824_-_JackMcLarney_-_TheBookOfBatchScriptingFromFundamentalsToAdvancedAutomation2024-06-24_-_Census.bat
@@ -1,9 +1,15 @@
 @ECHO OFF
 REM 2024-06-30T18:24:00 http://www.google.com/books/edition/The_Book_of_Batch_Scripting/SfLNEAAAQBAJ?hl=en&gbpv=1
 REM 2024-06-30T18:31:00 http://stackoverflow.com/questions/17605767/create-a-list-or-an-array-and-print-each-item-in-windows-batch
-set census=46500 59300
+
+set firstCensus=46500;^
+59300;^
+45650;^
+74600;^
+54400;
+
 set total=0
-(for %%a in (%census%) do (
+(for %%a in (%firstCensus%) do (
    echo %%a
    set /A total=total + %%a
 ))
