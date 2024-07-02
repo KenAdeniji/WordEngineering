df4e8d85a04306a2c62311abcbeb0510145a7c20
Ken Adeniji <kenadeniji@hotmail.com>
2024-07-01 21:04:40 -0700

    <p datetime="2024-07-01T18:27:00"> 				<a 					href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/GetAPage.html?word=How%20do%20we%20develop...%20our%20past%20time%3F" 					datetime="2024-07-01T18:27:00" 					data-ContactID="11253" 					data-HisWordID="163113" 					data-Commentary="Her father was sitting in their car in front of their house to the south." 				> 					How do we develop... our past time? 				</a> 			</p>

--- Batch/JackMcLarney/2024-06-30T1824_-_JackMcLarney_-_TheBookOfBatchScriptingFromFundamentalsToAdvancedAutomation2024-06-24_-_Census.bat
+++ Batch/JackMcLarney/2024-06-30T1824_-_JackMcLarney_-_TheBookOfBatchScriptingFromFundamentalsToAdvancedAutomation2024-06-24_-_Census.bat
@@ -2,6 +2,7 @@
 REM 2024-06-30T18:24:00 http://www.google.com/books/edition/The_Book_of_Batch_Scripting/SfLNEAAAQBAJ?hl=en&gbpv=1
 REM 2024-06-30T18:31:00 http://stackoverflow.com/questions/17605767/create-a-list-or-an-array-and-print-each-item-in-windows-batch
 
+set tribes=Reuben Simeon &REM census tribes
 set firstCensus=46500;^
 59300;^
 45650;^
@@ -16,9 +17,16 @@ set firstCensus=46500;^
 53400;
 
 set total=0
+set index=1
+REM equ or ==, neq, lss, leq, gtr, geq
+if "%index%" equ "1" (
+	echo Reuben
+)
 (for %%a in (%firstCensus%) do (
-   echo %%a
-   set /A total=total + %%a
+	echo %%a 
+	set /A index+=1
+	set /A total=total+%%a   
 ))
 echo %total%
 pause
+
