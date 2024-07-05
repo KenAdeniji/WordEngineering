e25913a2f1e1c7f362c832b7567d6323a6619704
Ken Adeniji <kenadeniji@hotmail.com>
2024-07-05 14:34:49 -0700

    <p id="InternationalBusinessMachinesIBM" datetime="2024-07-05T12:28:00"> 				International Business Machines (IBM): 				<ul> 					<li> 						<a 							href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/GetAPage.html?word=An%20association%20of%20being%20alike...%20is%20the%20assurance%20of%20the%20same." 							datetime="2024-07-05T12:53:00" 							data-HisWordID="163142" 						> 							An association of being alike... is the assurance of the same. 						</a> 					</li> 					<li> 						<a 							href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/GetAPage.html?word=The%20continuation%20of%20myself...%20is%20all%20I%20am%20allowed." 							datetime="2024-07-05T14:02:00" 							data-HisWordID="163143" 							data-ContactID="1000" 						> 							The continuation of myself... is all I am allowed. 						</a> 					</li> 				</ul> 				<ol> 					<li> 						IBM PC compatible:<br/> 						At Central Piedmont Community College (CPCC), Charlotte, North Carolina (NC). 						The author tutored in the personal computer (PC) laboratory. 						<ul> 							<li>The computers were IBM, Apple, Radio Shack</li> 							<li>The programming languages were Microsoft's Basic and Borland's Turbo Pascal</li> 							<li>The applications were WordStar wordprocessor, Lotus 1-2-3 spreadsheet, dBASE</li> 						</ul> 					</li> 					<li> 						IBM employee:<br/> 						<ul> 							<li>In Ilupeju, Lagos State, Nigeria, while the author was walking on the street, away from the premises of a client, Simeon Onasanya, an employee of IBM and the older brother of Tunde Onasanya, called him</li> 							<li>In Sydney, Australia, Alex Koutsombos, an IBM employee, sent periodic Java programming language e-mails</li> 						</ul> 					</li> 					<li> 						IBM event:<br/> 						<ul> 							<li>In Sydney, Australia, the author received an end-of-year gift of wine from IBM</li> 							<li>In Sydney, Australia, the author interviewed for a Microsoft SQL Server database administrator (DBA) role with IBM for the Sydney 2000 Olympics</li> 							<li>In San Francisco, California, the author attended an evening after-work party that was organized by IBM</li> 							<li>In San Jose, California, the author attended a conference that was hosted by IBM. Among the invitees were an African American mother and daughter</li> 						</ul> 					</li> 				</ol> 			</p>

--- Batch/2017-01-02DoingSomething.bat
+++ Batch/2017-01-02DoingSomething.bat
@@ -196,7 +196,7 @@ DBCC CHECKIDENT ('WordEngineering..ContactEmail', RESEED,  2878);
 DBCC CHECKIDENT ('WordEngineering..ContactURI', RESEED, 6501);
 DBCC CHECKIDENT ('WordEngineering..Dream', RESEED, 5138);
 DBCC CHECKIDENT ('WordEngineering..Event', RESEED, 1454);
-DBCC CHECKIDENT ('WordEngineering..HisWord', RESEED, 163088);
+DBCC CHECKIDENT ('WordEngineering..HisWord', RESEED, 163143);
 DBCC CHECKIDENT ('WordEngineering..QuestionAndAnswer', RESEED, 2963);
 DBCC CHECKIDENT ('WordEngineering..Remember', RESEED, 166205);
 DBCC CHECKIDENT ('WordEngineering..SacredText', RESEED, 631);
