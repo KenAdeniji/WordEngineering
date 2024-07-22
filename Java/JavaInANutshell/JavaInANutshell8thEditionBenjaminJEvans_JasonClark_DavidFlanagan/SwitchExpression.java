b9623b186b7949ecd1775c5d688f8aee3f6b3d8e
Ken Adeniji <kenadeniji@hotmail.com>
2024-07-21 21:09:19 -0700

    <h3 id="WhereIFeelWeHaveOpportunity" datetime="2024-07-21T19:07:21"> 				2024-07-21T19:07:21 President Joe Biden 				<a 					href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/GetAPage.html?word=Where%20I%20feel...%20we%20have%20opportunity%3F" 					data-ContactID="2443" 					data-HisWordID="163295" 					data-Location="I was walking on Paseo Padre Parkway just before Blackstone Way, south-east. At Dulhan Grocery Riya was wearing a yellow t-shirt and a pair of blue jeans trousers." 					data-url="apnews.com/article/biden-drops-out-2024-election-ddffde72838370032bdcff946cfc2ce6" 				> 					Where I feel... we have opportunity? 				</a> 			</h4> 			<p> 				<ul> 					<li datetime="2024-07-21T19:07:21"> 						Timeliness 						( 							<a 								href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/ScriptureReference.html?scriptureReference=Daniel%205:5,%20Daniel%205:11,%20Daniel%205:26,%20Daniel%205:30-31,%20Genesis%204:3,%20Genesis%204:8,%20Genesis%204:12-14" 							> 								Daniel 5:5, Daniel 5:11, Daniel 5:26, Daniel 5:30-31, Genesis 4:3, Genesis 4:8, Genesis 4:12-14 							</a> 						) 					</li> 					<li datetime="2024-07-21T19:07:21"> 						Quantification 						( 							<a 								href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/ScriptureReference.html?scriptureReference=Daniel%206:5" 							> 								Daniel 6:5 							</a> 						) 					</li> 				</ul> 			</p>

--- Java/JavaInANutshell/JavaInANutshell8thEditionBenjaminJEvans_JasonClark_DavidFlanagan/SwitchExpression.java
+++ Java/JavaInANutshell/JavaInANutshell8thEditionBenjaminJEvans_JasonClark_DavidFlanagan/SwitchExpression.java
@@ -3,14 +3,21 @@ public class SwitchExpression
 {
 	public static void main(String[] args) 
 	{
-		string input = "Y";
-		boolean yesOrNo = switch(input) {
+		boolean yesOrNo = switch(args[0]) {
 			case "y" -> true;
 			case "Y" -> true;
 			case "N" -> false;
 			case "n" -> false;
 			default -> throw new IllegalArgumentException("Y or N");
-		}
-		System.out.println(String.format("%s %b" , "input", yesOrNo)); 
+		};
+		System.out.println
+		(
+			String.format
+			(
+				"%s %b",
+				args[0],
+				yesOrNo
+			)
+		); 
 	}
 }
