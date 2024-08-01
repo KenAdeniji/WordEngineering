b290f1f0fe1f0265c918fba094c71cc2fb7222dc
Ken Adeniji <kenadeniji@hotmail.com>
2024-07-31 23:46:42 -0700

    git add web.config

--- GitHub.com/2023-02-21T0439GitHub.bat
+++ GitHub.com/2023-02-21T0439GitHub.bat
@@ -585,3 +585,5 @@ git add	        InformationInTransit/ItMustBeAbleToWorkAsAWholeAndAsItsEntireSel
 REM git rm -f		IIS/WordEngineering/2018-05-03Correspondence/2024-07-31T2239DulhanGrocery_CharterSquare_34139FremontBoulevard_34163FremontBoulevard_Fremont_California_94555_Patel_Taruben_Ayesha.txt
 
 git add			IIS/WordEngineering/2018-05-03Correspondence/2024-07-31T2239DulhanGrocery_CharterSquare_34139FremontBoulevard_34163FremontBoulevard_Fremont_California_94555_Patel_Taruben_Ayesha_Acts18v21.txt
+
+git add			IIS/WordEngineering/Web.config
