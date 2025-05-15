b70efb05dfc9357af93ba2d4da84271dcd6629ae
Ken Adeniji <KAdeniji@ephraimtech.com>
2025-05-14 17:39:48 -0700

    <h3 id="WhatDidHeMakeVariableWhatDidHeAssociateWithAge" data-datetime="2025-05-14T15:08:00"> 				2025-05-14T15:08:00 				<a 					href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/GetAPage.html?word=What%20did%20He%20make%20variable...%20what%20did%20He%20associate%20with%20age%3F" 					data-Dated="2025-05-14T15:08:00" 					data-HisWordID="166183" 					data-ContactID="7460" 				> 					What did He make variable... what did He associate with age? 				</a> 			</h3>
    			<ul>
    				<li>
    					Role: Army, Priest, King of the Jews
    					(
    						<a
    							href="http://e-comfort.ephraimtech.com/WordEngineering/WordUnion/ScriptureReference.html?scriptureReference=Numbers%201:3,%20Numbers%204:30,%20Numbers%208:24-25,%20Matthew%202:2"
    						>
    							Numbers 1:3, Numbers 4:30, Numbers 8:24-25, Matthew 2:2
    						</a>
    					)
    				</li>
    			</ul>

--- Batch/2017-01-02DoingSomething.bat
+++ Batch/2017-01-02DoingSomething.bat
@@ -187,7 +187,7 @@ DBCC CHECKIDENT ('URI..UriWordEngineering', RESEED, 71500);
 
 DBCC CHECKIDENT ('WordEngineering..ActToGod', RESEED, 2220);
 DBCC CHECKIDENT ('WordEngineering..APass', RESEED, 8865);
-DBCC CHECKIDENT ('WordEngineering..CaseBasedReasoning', RESEED, 161222);
+DBCC CHECKIDENT ('WordEngineering..CaseBasedReasoning', RESEED, 161376);
 DBCC CHECKIDENT ('WordEngineering..ClassAssociates', RESEED, 22952);
 DBCC CHECKIDENT ('WordEngineering..Contact', RESEED, 16055);
 DBCC CHECKIDENT ('WordEngineering..ContactEmail', RESEED,  2957);
