c1579e726674e9888fc730e0f3d8e8d0a61990f3
Ken Adeniji <KAdeniji@ephraimtech.com>
2025-05-27 16:57:12 -0700

    2025-05-27

--- IIS/WordEngineering/2025-05-27SeparateCommentaryPseudocode/2025-05-27SeparateCommentaryPseudocode.js
+++ IIS/WordEngineering/2025-05-27SeparateCommentaryPseudocode/2025-05-27SeparateCommentaryPseudocode.js
@@ -3,6 +3,7 @@
 	2025-05-27 	http://github.com/AllThingsSmitty/basic-design-patterns/blob/main/patterns/singleton/README.md
 	2025-05-27	http://stackoverflow.com/questions/33948829/how-do-i-use-jsdoc-on-windows
 	2025-05-27		npm install -g jsdoc
+	2025-05-27	http://stackoverflow.com/questions/9756120/how-do-i-get-a-utc-timestamp-in-javascript
 */
 /** Class representing the only Begotten Son. */
 class BegottenSon {
