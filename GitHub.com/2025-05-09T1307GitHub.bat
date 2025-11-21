122fc4b3336f00a549c6f78808e2cd13a28c02d0
Ken Adeniji <KAdeniji@ephraimtech.com>
2025-11-20 20:36:08 -0800

    2025-11-20T2009RobertC.Etheredge_-_academia.edu_50919962_JavaScript_Optimizing_native_JavaScript2025-11-20T2020WhoIsRedundant_Genesis7v15_Deuteronomy22_John17_Revelation13v18.txt

--- GitHub.com/2025-05-09T1307GitHub.bat
+++ GitHub.com/2025-05-09T1307GitHub.bat
@@ -392,5 +392,7 @@ REM git rm IIS/WordEngineering/2018-05-03Correspondence/2025-11-16T1114TimMahane
 
 git add	IIS/WordEngineering/Data/
 
-git add        IIS/WordEngineering/WordCross/2025-11-17DatabaseMetadata.asmx
-git add        IIS/WordEngineering/WordCross/2025-11-17DatabaseMetadata.html
+git add IIS/WordEngineering/WordCross/2025-11-17DatabaseMetadata.asmx
+git add IIS/WordEngineering/WordCross/2025-11-17DatabaseMetadata.html
+
+git add	IIS/WordEngineering/2018-05-03Correspondence/2025-11-20T2009RobertC.Etheredge_-_academia.edu_50919962_JavaScript_Optimizing_native_JavaScript2025-11-20T2020WhoIsRedundant_Genesis7v15_Deuteronomy22_John17_Revelation13v18.txt
