1bf3ed29ac04deee1195aaf791589373825f1c1e
Ken Adeniji <KAdeniji@HOLYSPIRIT.ephraimtech.com>
2026-02-12 23:42:46 -0800

    git mv "IIS/WordEngineering/2018-05-03Correspondence/2012-02-11DutchCourtInvestigateChipmakerNexperia_ChineseCEOSuspension_NijmegenNetherlands_Wingtech_ZhangXuezheng.txt" "IIS/WordEngineering/2018-05-03Correspondence/2026-02-11DutchCourtInvestigateChipmakerNexperia_ChineseCEOSuspension_NijmegenNetherlands_Wingtech_ZhangXuezheng.txt"

--- GitHub.com/2025-05-09T1307GitHub.bat
+++ GitHub.com/2025-05-09T1307GitHub.bat
@@ -519,7 +519,9 @@ git add IIS/WordEngineering/2018-05-03Correspondence/2026-02-06T1110Knowing...Is
 git add "IIS/WordEngineering/2018-05-03Correspondence/2026-02-106T1549HowToDateAPersonAccordingToTheSameResponseOnOurBelief_anthony.gcr1@cba.com.au_+61283885932.txt"
 git add "IIS/WordEngineering/Linux/2026-02-08WindowsSubsystemForLinux(WSL).txt"
 
-git add IIS/WordEngineering/2018-05-03Correspondence/2012-02-11DutchCourtInvestigateChipmakerNexperia_ChineseCEOSuspension_NijmegenNetherlands_Wingtech_ZhangXuezheng.txt
+rem git mv "IIS/WordEngineering/2018-05-03Correspondence/2012-02-11DutchCourtInvestigateChipmakerNexperia_ChineseCEOSuspension_NijmegenNetherlands_Wingtech_ZhangXuezheng.txt" "IIS/WordEngineering/2018-05-03Correspondence/2026-02-11DutchCourtInvestigateChipmakerNexperia_ChineseCEOSuspension_NijmegenNetherlands_Wingtech_ZhangXuezheng.txt"
+
+git add IIS/WordEngineering/2018-05-03Correspondence/2026-02-11DutchCourtInvestigateChipmakerNexperia_ChineseCEOSuspension_NijmegenNetherlands_Wingtech_ZhangXuezheng.txt
 
 git add InformationInTransit/ProcessCode/CustomizationOfClass.cs
 git add InformationInTransit/ProcessCode/CustomizationOfClass.exe
