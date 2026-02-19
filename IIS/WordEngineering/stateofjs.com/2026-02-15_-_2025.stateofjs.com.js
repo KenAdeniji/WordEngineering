44ca7b4ad22c26dc0f57d09902a1dabae2d0a6f8
Ken Adeniji <KAdeniji@ephraimtech.com>
2026-02-18 21:53:13 -0800

    http://2025.stateofjs.com/en-US/features/#syntax_features

--- IIS/WordEngineering/stateofjs.com/2026-02-15_-_2025.stateofjs.com.js
+++ IIS/WordEngineering/stateofjs.com/2026-02-15_-_2025.stateofjs.com.js
@@ -1,6 +1,17 @@
 export class StateOfJS2025 {
 	static	#privateField;
 	static 	version = "2025";
+	/**
+		Operators to assign a value to a variable based on its own truthy/falsy status.
+		2026-02-18 21:04:28.3633333	http://apnews.com/article/california-billionaires-bernie-sanders-gavin-newsom-democrats-87a1e54f463aad49a2093382969e5cca?utm_source=firefox-newtab-en-us	
+			2026-02-18T21:19:00 You can change the value... of a constant.
+				Matthew 17:24
+				2026-02-18T21:34:00	Attribute, property, field.
+	*/
+	static 	logicalAssignment(operandFirstField, secondOperand) {
+		operandFirstField ||= secondOperand;
+		return operandFirstField;
+	}
 	static 	nullishCoalescing(firstValue, secondValue) {
 		return firstValue ?? secondValue;
 	}
