44b693309282145454c34e8e7147e2d4be2f143e
Ken Adeniji <KAdeniji@HOLYSPIRIT.ephraimtech.com>
2026-04-04 16:05:46 -0700

    2026-04-04T15:42:00 When you welcome... exchange. Matthew 4:19, John 4:10

--- Batch/2017-01-02DoingSomething.bat
+++ Batch/2017-01-02DoingSomething.bat
@@ -1178,4 +1178,7 @@ http://kenadeniji.github.io/Github
 2026-04-03T18:42:00	http://stackoverflow.com/questions/27350405/how-to-get-the-holidays-date-in-my-case
 2026-04-03T20:33:00	http://stackoverflow.com/questions/2694640/find-an-element-in-dom-based-on-an-attribute-value
 
+1967-10-15...2026-10-29=21564 days (59 biblical years, 10 biblical months, 24 days) (59 years, 2 weeks)
+
+Sadie Crawford née Mobley
 :Exit
