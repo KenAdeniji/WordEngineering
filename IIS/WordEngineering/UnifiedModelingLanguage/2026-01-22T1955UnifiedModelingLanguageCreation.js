94fba0ac27a4777a27a7e8198deeb7f13f106925
Ken Adeniji <KAdeniji@HOLYSPIRIT.ephraimtech.com>
2026-04-30 14:56:34 -0700

    new Man("Joseph", "Jacob", "Rachel", "Dreamer", null),

--- IIS/WordEngineering/UnifiedModelingLanguage/2026-01-22T1955UnifiedModelingLanguageCreation.js
+++ IIS/WordEngineering/UnifiedModelingLanguage/2026-01-22T1955UnifiedModelingLanguageCreation.js
@@ -17,9 +17,12 @@
 	2026-04-29T16:34:00 new Man("Joseph", "Jacob", "Rachel", "Dreamer", null), Hakeem Olajuwon, the Dream of all basketball coaches
 	2026-04-29T16:52:00	http://mozilladatacollective.com/datasets?utm_source=mdn&utm_medium=paid&utm_campaign=open-data
 	2026-04-29T17:11:00	http://docs.github.com/en/actions/tutorials/create-actions/create-a-javascript-action
-	2026-04-30T10:46:00
+	2026-04-30T10:46:00	Natural primary key. get() method ... set() method.
 		this.men.find((element) => element.named === "Jacob")["correct"] = "Genesis 48:19";
 		this.men.find((element) => element.named === "Joseph")["correct"] = "Genesis 50:20, Genesis 39:8-9";
+		2026-04-30 12:26:11.033
+			Whoever practice sin is a slave of sin... the slave doesn't reside in the house, forever.	
+				John 8:34-35, Matthew 24:15-16, Mark 13:14, Luke 21:20-21
 */
 class Man {
 	constructor(named, father, mother, titled, vocation) {
@@ -34,6 +37,7 @@ class Man {
 	marriage() {}
 	knew() {}
 	sacrifice() {}
+	//2026-04-29T16:34:00
 	/*
 	correct() {
 		var scriptureReference = "";
@@ -64,8 +68,10 @@ this.men = [
 	new Man("Abel", "Adam", "Eve", null, "Animal breeder"),
 	new Man("Seth", "Adam", "Eve", "Substitute", "Call on the name of God"),
 	new Man("Enosh", "Seth", null, null, "Call on the name of God"),
+	new Man("Abraham", "Terah", null, null, "Father of many nations"),
 	new Man("Jacob", "Isaac", "Rebecca", "Israel - Prince with God", null),
 	new Man("Joseph", "Jacob", "Rachel", "Dreamer", null),
+	new Man("David", "Jesse", null, "A man after God's own heart", "A man of war"),
 ];
 
 /*
@@ -84,7 +90,16 @@ console.log
 );
 */
 
+//2026-04-30T10:46:00	Natural primary key. get() method ... set() method.
 this.men.find((element) => element.named === "Jacob")["correct"] = "Genesis 48:19";
 this.men.find((element) => element.named === "Joseph")["correct"] = "Genesis 50:20, Genesis 39:8-9";
+
+//2026-04-30T14:16:00	Out of him
+this.men.find((element) => element.named === "Adam")["outOfHim"] = "Genesis 2:23";
+
+this.men.find((element) => element.named === "Abraham")["heir"] = "Genesis 15:4";
+this.men.find((element) => element.named === "Abraham")["seed"] = "Genesis 21:12";
+
+this.men.find((element) => element.named === "David")["seed"] = "2 Samuel 7:12";
 
 console.log(this.men);
