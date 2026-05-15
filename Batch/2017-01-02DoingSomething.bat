03cb012c4b7066c128bdc09c1c1a6b2b041b8e78
Ken Adeniji <KAdeniji@HOLYSPIRIT.ephraimtech.com>
2026-05-14 23:23:41 -0700

    2026-05-14

--- Batch/2017-01-02DoingSomething.bat
+++ Batch/2017-01-02DoingSomething.bat
@@ -188,7 +188,7 @@ DBCC CHECKIDENT ('URI..UriPolitics', RESEED, 35);
 DBCC CHECKIDENT ('URI..UriTechnology', RESEED, 2);
 DBCC CHECKIDENT ('URI..UriWordEngineering', RESEED, 72017);
 
-DBCC CHECKIDENT ('WordEngineering..ActToGod', RESEED, 3768);
+DBCC CHECKIDENT ('WordEngineering..ActToGod', RESEED, 3891);
 DBCC CHECKIDENT ('WordEngineering..APass', RESEED, 10031);
 DBCC CHECKIDENT ('WordEngineering..CaseBasedReasoning', RESEED, 162698);
 DBCC CHECKIDENT ('WordEngineering..ClassAssociates', RESEED, 22952);
