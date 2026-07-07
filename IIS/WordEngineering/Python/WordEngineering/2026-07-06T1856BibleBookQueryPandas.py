005ce747f4ab16be9a0cadec2f3e46a0a598bd24
Ken Adeniji <KAdeniji@HOLYSPIRIT.ephraimtech.com>
2026-07-06 19:48:30 -0700

    Daniel 11:32

--- IIS/WordEngineering/Python/WordEngineering/2026-07-06T1856BibleBookQueryPandas.py
+++ IIS/WordEngineering/Python/WordEngineering/2026-07-06T1856BibleBookQueryPandas.py
@@ -1,6 +1,10 @@
 '''
 2026-07-06T18:51:00 http://www.w3schools.com/python/pandas/ref_df_query.asp
 http://github.com/KenAdeniji/WordEngineering/blob/main/IIS/WordEngineering/Python/WordEngineering/2026-07-06T1856BibleBookQueryPandas.py
+2026-07-06T19:40:00 Technology... where are you?
+    Technology will have to use artificial intelligence in-house, and build data centers.
+    Daniel 11:32
+    Urine, downstairs restroom, south-west.
 '''
 
 import pandas as pd
