fb2b065c11443880d7b75104bcc0a9d25c456019
Ken Adeniji <KAdeniji@HOLYSPIRIT.ephraimtech.com>
2026-08-17 23:52:29 -0700

    Notice the duplicate... Apocalyptic

--- IIS/WordEngineering/Python/DougHellmann/pymotw.com/Python3ModuleOfTheWeek/2026-08-17pymotw.com_-_Collections_ChainMap_OldTestament_NewTestament_Bible.py
+++ IIS/WordEngineering/Python/DougHellmann/pymotw.com/Python3ModuleOfTheWeek/2026-08-17pymotw.com_-_Collections_ChainMap_OldTestament_NewTestament_Bible.py
@@ -1,6 +1,7 @@
 #2026-08-17    http://pymotw.com/3/collections/chainmap.html
 """
     2026-08-17T23:01:00 Created.
+    2026-08-17T23:51:00 Notice the duplicate... Apocalyptic
 """
 import collections
 
@@ -11,7 +12,10 @@ new_testament = {"Gospel": "Matthew, Mark, Luke, John", "Apocalyptic": "Revelati
 bible = collections.ChainMap(old_testament, new_testament)
 
 print("Individual values")
-print("Pentateuch: {}".format(bible["Pentateuch"]))
-print("Gospel: {}".format(bible["Gospel"]))
-print("Apocalyptic: {}".format(bible["Apocalyptic"])) #Only Old Testament printed.
+print("Pentateuch: {}".format(bible["Pentateuch"])) #Pentateuch: Genesis, Exodus, Leviticus, Numbers, Deuteronomy
+print("Gospel: {}".format(bible["Gospel"])) #Gospel: Matthew, Mark, Luke, John
+print("Apocalyptic: {}".format(bible["Apocalyptic"])) #Keys: ['Gospel', 'Apocalyptic', 'Pentateuch']
 print()
+
+print("Keys: {}".format(list(bible.keys()))) #Keys: ['Gospel', 'Apocalyptic', 'Pentateuch']
+print("Values: {}".format(list(bible.values()))) #Values: ['Matthew, Mark, Luke, John', 'Daniel', 'Genesis, Exodus, Leviticus, Numbers, Deuteronomy']
